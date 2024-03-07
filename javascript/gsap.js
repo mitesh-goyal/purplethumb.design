9514ef6af71814b8e2876334171013177d091984
mitesh-goyal <146181863+mitesh-goyal@users.noreply.github.com>
2024-03-07 21:40:34 +0530

    Update gsap.js

--- javascript/gsap.js
+++ javascript/gsap.js
@@ -1,23 +1,22 @@
 let tempElement;
 tempElement = document.querySelector(".hero-banner-images");
 if (tempElement) {
-    gsap.fromTo(".hero-banner-images", {
-        xPercent: "120",
-    }, {
-
-        xPercent: "-360",
-        duration: 40,
+    gsap.to(".hero-banner-images", {
+        xPercent: "-300",
+        duration: 20,
         repeat: -1,
         ease: "linear",
     })
 }
 
+let timeline = gsap.timeline();
+
 tempElement = document.querySelector(".loader");
 if (tempElement) {
-    gsap.from('.loader', {
+    timeline.from('.loader', {
         top: "0%",
         duration: 1,
-        delay: 1,
+        delay: 0.2,
         stagger: 0.3,
-    })
+    }).set('.loaders-container', { display: "none" })
 }
